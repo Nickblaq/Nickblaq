2d18a23d5a3e1496431f4a5308f1267c0af3520c
nickblaq <nickookoro200@gmail.com>
2023-07-04 04:04:15 +0100

    beta 1.0.0

---
+++ utils/images.ts
@@ -0,0 +1,15 @@
+import v1 from '@/public/images/v1.avif'
+import v2 from '@/public/images/v1.avif'
+import v3 from '@/public/images/v1.avif'
+import v4 from '@/public/images/v1.avif'
+import v5 from '@/public/images/v1.avif'
+import v6 from '@/public/images/v1.avif'
+import v7 from '@/public/images/v1.avif'
+import v8 from '@/public/images/v1.avif'
+import v9 from '@/public/images/v1.avif'
+import v10 from '@/public/images/v1.avif'
+
+export default async function getResults() {
+    const images = [v1, v2, v3, v4, v5, v6, v7, v8, v9, v10];
+    return images;
+}
---
+++ utils/range.ts
@@ -0,0 +1,13 @@
+
+export const range = (start: number, end?: number) => {
+    let output: number[] = [];
+    if (typeof end === 'undefined') {
+        end = start;
+        start = 0;
+    }
+    for (let i: number = start; i < end; i += 1) {
+        output.push(i);
+    }
+    return output;
+}
+
